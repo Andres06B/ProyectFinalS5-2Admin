5e4505738e3bb5078990a6593a9458136676dca4
DxKaizer <solanoe934@gmail.com>
2024-11-20 22:04:40 -0500

    feature: Funcionamiento del login

--- src/app/service/admin/admin.service.ts
+++ src/app/service/admin/admin.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { jwtDecode, JwtPayload } from 'jwt-decode';
+
+
+interface MyJwtPayload extends JwtPayload {
+  nombre: string; 
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+
+  private baseUrl = 'http://localhost:8080/admin/login';
+  private token = 'auth-token';
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string) {
+    const user = { email, password };
+    return this.http.post<any>(this.baseUrl, user);
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem(this.token, token);
+  }
+
+  getDecodedToken(): MyJwtPayload | null {
+    const token = localStorage.getItem(this.token);
+    return token ? jwtDecode<MyJwtPayload>(token) : null;
+  }
+
 }
